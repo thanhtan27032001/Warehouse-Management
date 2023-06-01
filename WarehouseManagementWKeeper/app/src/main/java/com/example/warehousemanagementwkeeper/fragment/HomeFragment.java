dedbaa37394d23659bf7895a199ca6147f50b54f
thanhtan27032001 <90382807+thanhtan27032001@users.noreply.github.com>
2023-06-01 23:01:51 +0700

    no message

--- WarehouseManagementWKeeper/app/src/main/java/com/example/warehousemanagementwkeeper/fragment/HomeFragment.java
+++ WarehouseManagementWKeeper/app/src/main/java/com/example/warehousemanagementwkeeper/fragment/HomeFragment.java
@@ -2,18 +2,20 @@ package com.example.warehousemanagementwkeeper.fragment;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.warehousemanagementwkeeper.R;
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private LinearLayout cardCreateReceipt;
+    private LinearLayout cardImportManagement;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,22 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        cardCreateReceipt = view.findViewById(R.id.cardCreateReceipt);
+        cardImportManagement = view.findViewById(R.id.cardImportManagement);
+
+        setEvent();
+
+        return view;
+    }
+
+    private void setEvent() {
+        cardCreateReceipt.setOnClickListener(view -> {
+            Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
+        });
+        cardImportManagement.setOnClickListener(view -> {
+            Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
+        });
     }
 }
